07c0851033560d1d6d5c758917eb8fb85b7f8cfd
wankong <1549072621@qq.com>
2026-05-14 10:20:10 +0800

    feat: add _blank in Link

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,8 +1,12 @@
 import type { MDXComponents } from "mdx/types";
+import type { ComponentPropsWithoutRef } from "react";
 import { CodeBlock } from "@/components/mdx/code-block";
 import { ImageZoom } from "@/components/mdx/image-zoom";
 
 const components: MDXComponents = {
+	a: (props: ComponentPropsWithoutRef<"a">) => (
+		<a {...props} target="_blank" rel="noreferrer" />
+	),
 	img: ImageZoom,
 	pre: CodeBlock,
 };
